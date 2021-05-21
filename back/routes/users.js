57091fe79c1d42c4f2eb1b7dabaac6c0b4a03601
Spirkin S <s.spirkin@gmail.com>
2021-05-21 22:57:35 +0300

    обноляет данные

--- back/routes/users.js
+++ back/routes/users.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 
         return [
             {
-                title:'ДИНАМИКА ГОДОВЫХ ТЕМПОВ ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
+                title: 'ДИНАМИКА ГОДОВЫХ ТЕМПОВ ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
                 x: worksheet.worksheets[0].getColumn('A').values,
                 y: worksheet.worksheets[0].getColumn('B').values,
                 borderColor: '#FFBF00',
@@ -48,7 +48,233 @@ router.get('/', function (req, res, next) {
                 borderColor: '#40E0D0',
                 xTitle: 'Год',
                 yTitle: 'WWS'
+            },
+
+            {
+                title: 'КОЭФИЦИЕНТ ПЕРЕВОДА ИНВЕСТИЦИЙ В ОК ВО ВВОДЫ в сопоставимых ценах 1995 года ',
+                x: worksheet.worksheets[0].getColumn('AH').values,
+                y: worksheet.worksheets[0].getColumn('AI').values,
+                borderColor: '#FFA07A',
+                xTitle: 'Год',
+                yTitle: 'kS'
+            },
+
+            {
+                title: '  ДИНАМИКА  ГОДОВЫХ ТЕМПОВ ИНВЕСТИЦИЙ В ОСНОВНОЙ КАПИТАЛ',
+                x: worksheet.worksheets[0].getColumn('AO').values,
+                y: worksheet.worksheets[0].getColumn('AP').values,
+                borderColor: '#8B4513',
+                xTitle: 'Год',
+                yTitle: 'pIN'
+            },
+
+            {
+                title: '  ДИНАМИКА  БАЗИСНЫХ ТЕМПОВ ИНВЕСТИЦИЙ В ОСНОВНОЙ КАПИТАЛ',
+                x: worksheet.worksheets[0].getColumn('AU').values,
+                y: worksheet.worksheets[0].getColumn('AV').values,
+                borderColor: '#9ACD32',
+                xTitle: 'Год',
+                yTitle: 'Pn'
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВОГО ТЕМПА ИМПОРТА',
+                x: worksheet.worksheets[0].getColumn('BA').values,
+                y: worksheet.worksheets[0].getColumn('BB').values,
+                borderColor: '#FF1493',
+                xTitle: 'Год',
+                yTitle: 'pIM'
+            },
+
+            {
+                title: 'ДИНАМИКА БАЗИСНОГО ТЕМПА ИМПОРТА',
+                x: worksheet.worksheets[0].getColumn('BG').values,
+                y: worksheet.worksheets[0].getColumn('BH').values,
+                borderColor: '#0000FF',
+                xTitle: 'Год',
+                yTitle: 'Pm'
+            },
+
+            {
+                title: 'ДОЛЯ ИМПОРТА В ОТЕЧЕСТВЕННОМ ВЫПУСКЕ',
+                x: worksheet.worksheets[0].getColumn('BN').values,
+                y: worksheet.worksheets[0].getColumn('BO').values,
+                borderColor: '#D2691E',
+                xTitle: 'Год',
+                yTitle: 'qIM'
+            },
+
+            {
+                title: 'РЕГРЕССИЯ ВВОДОВ ОФ ОТ ИНВЕСТИЦИЙ В ОК в текущих рыночных ценах',
+                x: worksheet.worksheets[0].getColumn('BT').values,
+                y: worksheet.worksheets[0].getColumn('BU').values,
+                borderColor: '#FFD700',
+                xTitle: 'Год',
+                yTitle: 'WW'
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВЫХ ТЕМПОВ КОНЕЧНОГО ПОТРЕБЛЕНИЯ ДОМАШНИХ ХОЗЯЙСТВ',
+                x: worksheet.worksheets[0].getColumn('CA').values,
+                y: worksheet.worksheets[0].getColumn('CB').values,
+                borderColor: '#808000',
+                xTitle: 'Год',
+                yTitle: 'pYD'
+            },
+
+            {
+                title: 'ДИНАМИКА  БАЗИСНЫХ ТЕМПОВ КОНЕЧНОГО ПОТРЕБЛЕНИЯ ДОМАШНИХ ХОЗЯЙСТВ',
+                x: worksheet.worksheets[0].getColumn('CG').values,
+                y: worksheet.worksheets[0].getColumn('CH').values,
+                borderColor: '#20B2AA',
+                xTitle: 'Год',
+                yTitle: 'Pd'
+            },
+
+            {
+                title: 'ДИНАМИКА  ЧИСЛЕННОСТИ ЗАНЯТЫХ И БЕЗРАБОТНЫХ (млн чел)',
+                x: worksheet.worksheets[0].getColumn('CN').values,
+                y: worksheet.worksheets[0].getColumn('CO').values,
+                borderColor: '#C71585',
+                xTitle: 'Год',
+                yTitle: 'LZ'
+            },
+
+            {
+                title: 'ВЫПУСК ОТЕЧЕСТВЕННОЙ ПРОДУКЦИИ в сопоставимых ценах 1995 года',
+                x: worksheet.worksheets[0].getColumn('CV').values,
+                y: worksheet.worksheets[0].getColumn('CW').values,
+                borderColor: '#FF4500',
+                xTitle: 'Год',
+                yTitle: 'XOS (млрд руб)'
+            },
+
+            {
+                title: 'ДИНАМИКА ПРОИЗВОДИТЕЛЬНОСТИ ТРУДА в сопоставимых ценах 1995 года (тыс руб / чел)',
+                x: worksheet.worksheets[0].getColumn('DB').values,
+                y: worksheet.worksheets[0].getColumn('DC').values,
+                borderColor: '#00FF00',
+                xTitle: 'Год',
+                yTitle: 'POS = XOS / LZ'
+            },
+
+            {
+                title: 'БАЗИСНЫЕ ДЕФЛЯТОРЫ ВВОДОВ ОФ ИНВЕСТИЦИЙ в ОК и ОСНОВЫНЫХ ФОНДОВ',
+                x: worksheet.worksheets[0].getColumn('DH').values,
+                y: worksheet.worksheets[0].getColumn('DI').values,
+                borderColor: '#FF0000',
+                xTitle: 'Год',
+                yTitle: 'Dww'
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВЫХ ТЕМПОВ КОНЕЧНОГО ПОТРЕБЛЕНИЯ ГОСУДАРСТВА',
+                x: worksheet.worksheets[0].getColumn('DP').values,
+                y: worksheet.worksheets[0].getColumn('DQ').values,
+                borderColor: '#00CED1',
+                xTitle: 'Год',
+                yTitle: 'pYG'
+            },
+
+            {
+                title: 'ДИНАМИКА  БАЗИСНОГО ТЕМПА КОНЕЧНОГО ПОТРЕБЛЕНИЯ ГОСУДАРСТВА',
+                x: worksheet.worksheets[0].getColumn('DV').values,
+                y: worksheet.worksheets[0].getColumn('DW').values,
+                borderColor: '#800000',
+                xTitle: 'Год',
+                yTitle: 'Pg'
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВОГО ТЕМПА ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
+                x: worksheet.worksheets[0].getColumn('EB').values,
+                y: worksheet.worksheets[0].getColumn('EC').values,
+                borderColor: '#808000',
+                xTitle: 'Год',
+                yTitle: ''
+            },
+
+            {
+                title: 'ДИНАМИКА ОСНОВНЫХ ФОНДОВ в сопоставимых ценах 1995 года (млрд руб)',
+                x: worksheet.worksheets[0].getColumn('EH').values,
+                y: worksheet.worksheets[0].getColumn('EI').values,
+                borderColor: '#B8860B',
+                xTitle: 'Год',
+                yTitle: 'FS'
+            },
+
+            {
+                title: 'ДИНАМИКА ФОНДООТДАЧИ в сопоставимых ценах 1995 года',
+                x: worksheet.worksheets[0].getColumn('EO').values,
+                y: worksheet.worksheets[0].getColumn('EP').values,
+                borderColor: '#800080',
+                xTitle: 'Год',
+                yTitle: 'FOS=XOS/FS'
+            },
+
+            {
+                title: 'ДИНАМИКА  БАЗИСНОГО ТЕМПА ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
+                x: worksheet.worksheets[0].getColumn('EU').values,
+                y: worksheet.worksheets[0].getColumn('EV').values,
+                borderColor: '#FFD700',
+                xTitle: 'Год',
+                yTitle: ''
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВЫХ ТЕМПОВ КОНЕЧНОГО ПОТРЕБЛЕНИЯ НКО',
+                x: worksheet.worksheets[0].getColumn('FA').values,
+                y: worksheet.worksheets[0].getColumn('FB').values,
+                borderColor: '#8B0000',
+                xTitle: 'Год',
+                yTitle: 'pYNK'
+            },
+
+            {
+                title: 'ДИНАМИКА  БАЗИСНОГО ТЕМПА КОНЕЧНОГО ПОТРЕБЛЕНИЯ НКО',
+                x: worksheet.worksheets[0].getColumn('FH').values,
+                y: worksheet.worksheets[0].getColumn('FI').values,
+                borderColor: '#00FFFF',
+                xTitle: 'Год',
+                yTitle: 'Pnk'
+            },
+
+            {
+                title: 'ДИНАМИКА  ГОДОВОГО ТЕМПА ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
+                x: worksheet.worksheets[0].getColumn('FN').values,
+                y: worksheet.worksheets[0].getColumn('FO').values,
+                borderColor: '#D2691E',
+                xTitle: 'Год',
+                yTitle: ''
+            },
+
+            {
+                title: 'ГРАНИЦА ОТЕЧЕСТВЕННОГО ВЫПУСКА РОССИЙСКОЙ ЭКОНОМИКИ в сопоставимых ценах 1995 года',
+                x: worksheet.worksheets[0].getColumn('FT').values,
+                y: worksheet.worksheets[0].getColumn('FU').values,
+                borderColor: '#2F4F4F',
+                xTitle: 'Год',
+                yTitle: 'XOS'
+            },
+
+            {
+                title: 'КОЭФФИЦИЕНТ ЗАГРУЗКИ ПРОИЗВОДСТВЕННЫХ  МОЩНОСТЕЙ',
+                x: worksheet.worksheets[0].getColumn('GA').values,
+                y: worksheet.worksheets[0].getColumn('GB').values,
+                borderColor: '#00FF00',
+                xTitle: 'Год',
+                yTitle: 'kZG'
+            },
+
+            {
+                title: 'ДИНАМИКА  БАЗИСНОГО ТЕМПА ВАЛОВОГО ВНУТРЕННЕГО ПРОДУКТА',
+                x: worksheet.worksheets[0].getColumn('GG').values,
+                y: worksheet.worksheets[0].getColumn('GH').values,
+                borderColor: '#FF0000',
+                xTitle: 'Год',
+                yTitle: ''
             }
+
         ];
     }
 
@@ -69,7 +295,6 @@ router.post('/', async function (req, res, next) {
         console.log('Finished...');
     });
 });
-
 
 
 module.exports = router;
